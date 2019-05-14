c4e008626ffe98938320e7e42fe5a61181059ed3
pelmenits <50447177+pelmenits@users.noreply.github.com>
2019-05-14 10:38:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+function atzime(cipars)
+{
+    if(cipars == 1)
+    {
+        return "😭";
+    }
+    if(cipars == 2)
+    {
+        return"😥";
+    }
+    if(cipars == 3)
+    {
+        return"😰";
+    }
+    if(cipars == 4)
+    {
+        return"😲";
+    }
+    if(cipars == 5)
+    {
+        return"😩";
+    }
+    if(cipars == 6)
+    {
+        return"😦";
+    }
+    if(cipars == 7)
+    {
+        return"🍆";
+    }
+    if(cipars == 8)
+    {
+        return"😃";
+    }
+    if(cipars == 9)
+    {
+        return"😂";
+    }
+    if(cipars == 10)
+    {
+        return"😎";
+    }
+
+
+
+
+}
+console.log(atzime(10));
